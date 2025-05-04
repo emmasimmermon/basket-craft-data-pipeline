f08b2ff171971dd025593fd0c3a6002679bc35e2
emmasimmermon <156370222+emmasimmermon@users.noreply.github.com>
2025-05-04 01:16:54 +0000

    changes

--- elt/basket_craft_orders_extract_load_raw.py
+++ elt/basket_craft_orders_extract_load_raw.py
@@ -2,8 +2,8 @@
 1. Import necessary libraries.
 2. Load source MySQL and destination Postgres connection details.
 3. Build connection strings and create database engines.
-4. Read products table from MySQL and load into a DataFrame.
-5. Write DataFrame to products table in Postgres (raw schema).
+4. Read orders table from MySQL and load into a DataFrame.
+5. Write DataFrame to orders table in Postgres (raw schema).
 """
 
 # %%
@@ -41,18 +41,18 @@ mysql_engine = create_engine(mysql_conn_str)
 pg_engine = create_engine(pg_conn_str)
  
 # %%
-# Read products table from MySQL and load into a DataFrame
-df = pd.read_sql('SELECT * FROM products', mysql_engine)
+# Read orders table from MySQL and load into a DataFrame
+df = pd.read_sql('SELECT * FROM orders', mysql_engine)
  
 # %%
 # df
  
 # %%
-# Write DataFrame to products table in Postgres (raw schema)
-df.to_sql('products', pg_engine, schema='raw', if_exists='replace', index=False)
+# Write DataFrame to orders table in Postgres (raw schema)
+df.to_sql('orders', pg_engine, schema='raw', if_exists='replace', index=False)
  
 # %%
-print(f'{len(df)} records loaded into Postgres products table.')
+print(f'{len(df)} records loaded into Postgres orders table.')
  
 # %%
 df
